d0e65e131df1a054dc61e5e17bf16287449cdd1f
Mariusz Błaszczak <mariusz.blaszczak@akra.net>
2018-12-03 14:18:27 +0100

    install cypress, add failing test(possible bug), add workaround test

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -1,3 +1,34 @@
 describe('page', () => {
-  it('works', () => {})
+  it('this should work, but does not', () => {
+    cy.visit('https://getbootstrap.com/docs/4.1/components/modal/')
+
+    cy.get("body > div > div > main > div:nth-child(20) > button").as('open_modal_btn')
+    cy.get('#exampleModalLive > div > div > div.modal-header > button span').as('close_modal_btn')
+
+    cy.get('@open_modal_btn').scrollIntoView().click()
+
+    cy.get('@close_modal_btn').click()
+
+    cy.get('@open_modal_btn').scrollIntoView().click()
+    cy.get('@close_modal_btn').click()
+  })
+
+  it('this is workaround', () => {
+    cy.visit('https://getbootstrap.com/docs/4.1/components/modal/')
+
+    cy.get("body > div > div > main > div:nth-child(20) > button").as('open_modal_btn')
+    cy.get('#exampleModalLive > div > div > div.modal-header > button span').as('close_modal_btn')
+    const number_of_wait = 200;
+
+    cy.get('@open_modal_btn').scrollIntoView().click()
+
+    cy.wait(number_of_wait)
+
+    cy.get('@close_modal_btn').click()
+
+    cy.wait(number_of_wait)
+
+    cy.get('@open_modal_btn').scrollIntoView().click()
+    cy.get('@close_modal_btn').click()
+  })
 })
